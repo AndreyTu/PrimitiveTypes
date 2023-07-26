579ea9df72d7be1b1fc619851bd7ccc030f60b42
AndreyT <zanarions@gmail.com>
2023-07-26 10:22:59 +0300

    Change

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int cost = 15000;
+        int rub = 20;
+        int bonus = cost/rub;
+
+        System.out.println("Итоговый бонус: " + bonus + " миль");
+    }
+}
